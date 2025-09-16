f4b5f2d7680de768fb106d265692c135a28c75f0
Thecla Owano <teckowano@gmail.com>
2025-09-16 10:05:48 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(a, b) {
+    return a / b;
+};
+console.log(divide(2000, 100)); // Output: 5
+
+const square = (num) => num * num;
+console.log(square(5)); // Output: 25
+
+const add = (x, y) => x + y;
+console.log(add(3, 7)); // Output: 10
